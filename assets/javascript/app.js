e604d0bd3cd4480bb59b6f98ce9456e5c3d0b58e
Eric LaMacchia <ericlamacchia1@icloud.com>
2019-09-15 15:04:08 -0400

    Added functionality to be able to delete the user searches from the page, this was difficult and required a good deal of research.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,19 @@
 // Need to create my variables and my array to hold the catergories
-const buttons = ["Dogs", "Cats", "Babies", "Turtles"];
+let buttons = ["Dogs", "Cats", "Babies", "Turtles"];
+// Need to create a function for keeping the user added search criteria when the page is reloaded
+function loadButtons() {
+    const listButtons = JSON.parse(localStorage.getItem('buttons'));
+    buttons = listButtons;
+}
 
 // Need to make a function that will make the buttons for each category to appear
 
 function renderButton() {
     $('.recent-search').empty();
-    
-   for (let i = 0; i < buttons.length; i++) {
-       const buttonName = buttons[i];
-       const button = `
+
+    for (let i = 0; i < buttons.length; i++) {
+        const buttonName = buttons[i];
+        const button = `
        <div class="wrap-buttons">
        <button 
        class="btn btn-search"
@@ -18,22 +23,35 @@ function renderButton() {
        <button
        
        data-name="${buttonName}"
+       data-index="${i}"
+       class="btn btn-delete fas fa-times-circle">
+       
 
-       class="btn btn-delete fas fa-times-circle"
-       >
+       
+       
        </button>
         </div>
        `;
 
-       $('.recent-search').append(button);
-   } 
+        $('.recent-search').append(button);
+    }
+    // Need to add a function that will retain user inputed searches into local storage
+    localStorage.setItem('buttons', JSON.stringify(buttons));
 }
+// Need to add a function to make a new button for each new user search
+loadButtons();
 renderButton();
-$('#submit-button').on('click', function(event) {
+$('#submit-button').on('click', function (event) {
     event.preventDefault();
-   const value = $('#search').val();
-   buttons.push(value);
-   renderButton();
-   console.log('Value: ', value);
-});
+    const value = $('#search').val();
+    buttons.push(value);
+    renderButton();
 
+});
+$(document).on('click', '.btn-delete', function () {
+    const buttonIndex = $(this).attr('data-index');
+    buttons.splice(buttonIndex, 1);
+    renderButton();
+    console.log('Button Index: ', buttonIndex);
+
+});
